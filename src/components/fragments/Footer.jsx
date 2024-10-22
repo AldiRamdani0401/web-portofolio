022538702a457961272d2418e2e21d471a2a0ff0
Aldi Ramdani <ramdanialdi889@gmail.com>
2024-10-22 21:34:22 +0700

    fix layout 1

--- src/components/fragments/Footer.jsx
+++ src/components/fragments/Footer.jsx
@@ -1,13 +1,21 @@
-import ListMenu from "./ListMenu";
+import hero from "../../assets/images/hero.png";
+
+const date = new Date();
+const thisYear = date.getFullYear();
 
 const Footer = () => {
   return (
-    <footer className="flex flex-row gap-4 justify-between bg-gray-950 p-4 w-full text-white">
-      <a href="#" class=" cursor-pointer text-lg font-bold">MyPortofolio</a>
-      <div className="sm:hidden lg:block">
-        <ListMenu />
+    <footer className="flex flex-col gap-4 justify-between bg-gray-950 p-4 w-full text-white">
+      <div className="flex flex-row gap-4 justify-end w-full">
+        <a class="sm:hidden lg:block hover:font-bold hover:text-lg cursor-pointer">Join Me</a>
       </div>
-      <a class="sm:hidden lg:block hover:font-bold hover:text-lg cursor-pointer">Join Me</a>
+      <div className="flex flex-row gap-4 justify-between w-full">
+        <a href="#" class=" cursor-pointer text-lg font-bold">MyPortofolio</a>
+        <div className="sm:hidden lg:block">
+          Copyright &copy; {thisYear} - Aldi Ramdani <img src={hero} class="bg-white rounded-full w-[35px] h-[35px] inline ml-2" />
+        </div>
+        <a class="sm:hidden lg:block hover:font-bold hover:text-lg cursor-pointer">Join Me</a>
+      </div>
     </footer>
   );
 }
