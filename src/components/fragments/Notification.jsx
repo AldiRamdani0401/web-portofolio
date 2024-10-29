9d62334da4c0042c1c3e9e218990c614735b8e34
Aldi Ramdani <ramdanialdi889@gmail.com>
2024-10-29 16:01:44 +0700

    fix notification

--- src/components/fragments/Notification.jsx
+++ src/components/fragments/Notification.jsx
@@ -5,7 +5,7 @@ const Notification = () => {
 
   return (
     <div className={`${visible() ? 'flex' : 'hidden'} justify-center items-center gap-10 sticky top-0 w-full text-center font-bold bg-yellow-500 py-2 animate-pulse`}>
-      <h1 className="self-center text-2xl">👷🏻‍♂️ Under Development 👷🏻‍♂️</h1>
+      <h1 className="self-center text-xs lg:text-2xl text-nowrap">👷🏻‍♂️ This website is still under development 👷🏻‍♂️</h1>
       <button
         className="rounded-lg bg-red-600 hover:bg-red-400 text-white font-light hover:font-bold px-2 py-1"
         onClick={() => setVisible((prev) => !prev)}
