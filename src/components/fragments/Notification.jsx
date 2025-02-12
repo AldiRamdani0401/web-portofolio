4bdb3aafe1d85a7ca7beb0c15a4183f21f45056d
AldiRamdani0401 <ramdanialdi889@gmail.com>
2025-02-12 11:10:08 +0700

    fix: notification

--- src/components/fragments/Notification.jsx
+++ src/components/fragments/Notification.jsx
@@ -1,17 +1,38 @@
 import { createSignal } from "solid-js";
+import { developmentGif } from "../../assets/gif";
 
 const Notification = () => {
   const [visible, setVisible] = createSignal(true);
 
   return (
-    <div className={`${visible() ? 'flex' : 'hidden'} justify-center items-center gap-10 sticky top-0 w-full text-center font-bold bg-yellow-500 py-2 animate-pulse`}>
-      <h1 className="self-center text-xs lg:text-2xl text-nowrap">👷🏻‍♂️ This website is still under development 👷🏻‍♂️</h1>
-      <button
-        className="rounded-lg bg-red-600 hover:bg-red-400 text-white font-light hover:font-bold px-2 py-1"
+    <div
+      className={`${
+        visible() ? "flex" : "hidden"
+      } justify-center items-center gap-10 sticky top-0 w-full text-center bg-yellow-500 py-2`}
+    >
+      <h1 className="self-center flex items-center gap-2 text-sm lg:text-2xl font-medium text-slate-700 text-nowrap">
+        <img
+          src={developmentGif}
+          className="h-10 w-10 rounded-full"
+          alt="development icon"
+        />{" "}
+        This website is still under development{" "}
+      </h1>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="2em"
+        height="2em"
+        className="bg-red-50 rounded-full hover:bg-red-100 text-red-600 hover:text-red-500"
         onClick={() => setVisible((prev) => !prev)}
       >
-        X
-      </button>
+        <path
+          fill="currentColor"
+          fillRule="evenodd"
+          d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12m7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586z"
+          clipRule="evenodd"
+        ></path>
+      </svg>
     </div>
   );
 };
